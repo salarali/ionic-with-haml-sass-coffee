98ce575ddbdade4fbc384af81b182baa0e832bf1
Salar Ali Mumtaz <salaarali@gmail.com>
2015-02-08 12:11:05 -0500

    Add all the dependencies for gulp Update the name of the project

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,27 +1,15 @@
-angular.module('starter.controllers', [])
-
-.controller('DashCtrl', function($scope) {})
-
-.controller('ChatsCtrl', function($scope, Chats) {
+angular.module("starter.controllers", []).controller("DashCtrl", function($scope) {}).controller("ChatsCtrl", function($scope, Chats) {
   $scope.chats = Chats.all();
   $scope.remove = function(chat) {
     Chats.remove(chat);
-  }
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  };
+}).controller("ChatDetailCtrl", function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('FriendsCtrl', function($scope, Friends) {
+}).controller("FriendsCtrl", function($scope, Friends) {
   $scope.friends = Friends.all();
-})
-
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+}).controller("FriendDetailCtrl", function($scope, $stateParams, Friends) {
   $scope.friend = Friends.get($stateParams.friendId);
-})
-
-.controller('AccountCtrl', function($scope) {
+}).controller("AccountCtrl", function($scope) {
   $scope.settings = {
     enableFriends: true
   };
